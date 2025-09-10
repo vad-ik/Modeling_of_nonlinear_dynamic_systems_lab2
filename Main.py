40b3a538644a5876f68c551c6ca49a2aa9f57d4f
vadim <7vadim7spb2003@gmail.com>
2025-09-10 12:28:41 +0300

    full main

--- Main.py
+++ Main.py
@@ -7,22 +7,22 @@ from RungeKutta2 import RungeKutta2
 from RungeKutta4 import RungeKutta4
 
 
-def solve(metod,time,h,X,a,rossler,name):
-    X1=metod.getAns(time,h,X,a,rossler)
-    Painter.draw(X1,h,name)
-    Painter.draw_3d(X1,name)
-
-def getDeviation(metod,time,h,X,a,rossler,name):
+def solve(metod, time, h, X, a, rossler, name):
     X1 = metod.getAns(time, h, X, a, rossler)
-    X2= RungeKutta4().getAns(time, h, X, a, rossler)
+    Painter.draw(X1, h, name)
+    Painter.draw_3d(X1, name)
+
+
+def getDeviation(metod, time, h, X, a, rossler, name):
+    X1 = metod.getAns(time, h, X, a, rossler)
+    X2 = RungeKutta4().getAns(time, h, X, a, rossler)
     X1_np = np.array(X1)
     X2_np = np.array(X2)
 
     # Разница между массивами
     difference = X2_np - X1_np
-    Painter.draw(difference, h, name,True)
+    Painter.draw(difference, h, name, True)
     Painter.draw_3d(difference, name)
-
 
 
 if __name__ == "__main__":
@@ -32,9 +32,9 @@ if __name__ == "__main__":
     time = 100
     h = 0.0001
 
-    # solve(Eiler(),time,h,X,a,rossler,"Метод эйлера")
-    # solve(RungeKutta2(),time,h,X,a,rossler,"Неявный метод Рунге — Кутты второго порядка")
-    # solve(RungeKutta4(),time,h,X,a,rossler,"Классический метод Рунге — Кутты четвёртого порядка")
-    # getDeviation(RungeKutta2(),time,h,X,a,rossler,"Неявный метод Рунге — Кутты второго порядка")
-    getDeviation(Eiler(),time,h,X,a,rossler,"Метод эйлера")
-    #
+    solve(Eiler(), time, h, X, a, rossler, "Метод эйлера")
+    solve(RungeKutta2(), time, h, X, a, rossler, "Неявный метод Рунге — Кутты второго порядка")
+    solve(RungeKutta4(), time, h, X, a, rossler, "Классический метод Рунге — Кутты четвёртого порядка")
+    getDeviation(RungeKutta2(), time, h, X, a, rossler, "Неявный метод Рунге — Кутты второго порядка")
+    getDeviation(Eiler(), time, h, X, a, rossler, "Метод эйлера")
+#
