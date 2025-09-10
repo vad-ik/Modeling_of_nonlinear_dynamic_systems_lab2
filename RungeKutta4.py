03b772dc33d5d1e6b1716451efe8c0ba76fabdae
vadim <7vadim7spb2003@gmail.com>
2025-09-10 12:27:17 +0300

    fix rk4

--- RungeKutta4.py
+++ RungeKutta4.py
@@ -1,28 +1,52 @@
 class RungeKutta4:
     def getAns(self, time, h, X, a, func):
         steps = int(time / h) + 1
-        X1 = [[0.0] * 3 for _ in range(steps)]  # 3 столбца (x, y, z)
+        X1 = [[0.0] * 3 for _ in range(steps)]
 
         X1[0] = X.copy()
 
         for i in range(steps - 1):
-            k = [[0.0, 0.0, 0.0],
-                 [0.0, 0.0, 0.0],
-                 [0.0, 0.0, 0.0],
-                 [0.0, 0.0, 0.0]]
+            k1 = [
+                func.getdX(X1[i], a),
+                func.getdY(X1[i], a),
+                func.getdZ(X1[i], a)
+            ]
 
-            for l in range(4):
-                Xtemp = X1[i].copy()
-                k[l][0] = func.getdX(X1[i], a)
-                k[l][1] = func.getdY(X1[i], a)
-                k[l][2] = func.getdZ(X1[i], a)
+            X_temp_k2 = [
+                X1[i][0] + 0.5 * h * k1[0],
+                X1[i][1] + 0.5 * h * k1[1],
+                X1[i][2] + 0.5 * h * k1[2]
+            ]
+            k2 = [
+                func.getdX(X_temp_k2, a),
+                func.getdY(X_temp_k2, a),
+                func.getdZ(X_temp_k2, a)
+            ]
 
-                for j in range(3):
-                    Xtemp[j] = Xtemp[j] + 0.5 * h * k[l][j]
+            X_temp_k3 = [
+                X1[i][0] + 0.5 * h * k2[0],
+                X1[i][1] + 0.5 * h * k2[1],
+                X1[i][2] + 0.5 * h * k2[2]
+            ]
+            k3 = [
+                func.getdX(X_temp_k3, a),
+                func.getdY(X_temp_k3, a),
+                func.getdZ(X_temp_k3, a)
+            ]
+
+            X_temp_k4 = [
+                X1[i][0] + h * k3[0],
+                X1[i][1] + h * k3[1],
+                X1[i][2] + h * k3[2]
+            ]
+            k4 = [
+                func.getdX(X_temp_k4, a),
+                func.getdY(X_temp_k4, a),
+                func.getdZ(X_temp_k4, a)
+            ]
 
             for j in range(3):
-                X1[i + 1][j] = X1[i][j] + h / 6 * (k[0][j] + 2 * k[1][j] + 2 * k[2][j] + k[3][j])
-
+                X1[i + 1][j] = X1[i][j] + (h / 6.0) * (k1[j] + 2 * k2[j] + 2 * k3[j] + k4[j])
         X_coords, Y_coords, Z_coords = zip(*X1)
 
         return [list(X_coords), list(Y_coords), list(Z_coords)]
